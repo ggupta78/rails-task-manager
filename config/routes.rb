10c00918b365ecb7f1ec25f07b3ba9f02dd0b001
Gautam Gupta <ggupta78@gmail.com>
2022-11-08 15:09:10 +0800

    complete all functionality and refactoring but not bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,8 @@ Rails.application.routes.draw do
   get '/tasks', to: 'tasks#index'
   post '/tasks', to: 'tasks#create'
   get '/tasks/new', to: 'tasks#new'
-  get '/tasks/:id', to: 'tasks#show', as: :show
+  get '/tasks/:id', to: 'tasks#show', as: :task
+  patch '/tasks/:id', to: 'tasks#update'
+  delete '/tasks/:id', to: 'tasks#destroy'
+  get '/tasks/:id/edit', to: 'tasks#edit', as: :edit_task
 end
