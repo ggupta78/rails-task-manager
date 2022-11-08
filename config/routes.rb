5a75c49fdb942f0a88f91322bfbe5e2f8e21802f
Gautam Gupta <ggupta78@gmail.com>
2022-11-08 13:00:21 +0800

    add the show feature and starting new

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root 'tasks#index'
+  get '/tasks', to: 'tasks#index'
+  get '/tasks/new', to: 'tasks#new'
+  get '/tasks/:id', to: 'tasks#show', as: :show
 end
